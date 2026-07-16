9e6c3a2d5dfb4b3deef3e61c43a30a10460b8520
ssb-stud-lng <stud-lng@ssb.no>
2026-07-16 13:56:25 +0200

    lagd et program

---
+++ examples/world.py
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.3
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+print("world")
